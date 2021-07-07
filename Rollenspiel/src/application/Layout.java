5030ec160ef1f38f0891bcf81aef78a535ce2183
Forsa1997 <54152308+Forsa1997@users.noreply.github.com>
2021-07-07 12:18:37 +0200

    Erste Versuche ein Layout zu erstellen.

---
+++ Rollenspiel/src/application/Layout.java
@@ -0,0 +1,86 @@
+package application;
+
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Layout {
+
+	VBox leftBox = new VBox();
+	VBox rightBox = new VBox();
+	VBox combatView = new VBox();
+	VBox outputView = new VBox();
+	VBox statsView = new VBox();
+	VBox selectionView = new VBox();
+
+	public Layout() {
+
+	}
+
+	public Scene createScene() {
+
+		changeBackgrounds();
+
+		this.leftBox.getChildren().addAll(this.combatView, this.outputView);
+		this.rightBox.getChildren().addAll(this.statsView, this.selectionView);
+		Rectangle rectangle = new Rectangle(700, 700);
+		Rectangle rectangle1 = new Rectangle(700, 300);
+		Rectangle rectangle2 = new Rectangle(300, 700);
+		Rectangle rectangle3 = new Rectangle(300, 300);
+		rectangle.setFill(Color.ORANGE);
+		rectangle1.setFill(Color.RED);
+		rectangle2.setFill(Color.BLUE);
+		rectangle3.setFill(Color.GREEN);
+		Button testButton = new Button("Test");
+		Button testButton1 = new Button("Test1");
+		Button testButton2 = new Button("Test2");
+		Button testButton3 = new Button("Test3");
+		this.combatView.getChildren().add(testButton);
+		this.outputView.getChildren().add(testButton1);
+		this.statsView.getChildren().add(testButton2);
+		this.selectionView.getChildren().add(testButton3);
+		this.combatView.getChildren().add(rectangle);
+		this.outputView.getChildren().add(rectangle1);
+		this.statsView.getChildren().add(rectangle2);
+		this.selectionView.getChildren().add(rectangle3);
+
+		HBox root = new HBox();
+		root.getChildren().addAll(this.leftBox, this.rightBox);
+		Scene scene = new Scene(root, 1000, 1000);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+		return scene;
+
+	}
+
+	private void changeBackgrounds() {
+
+		this.combatView
+				.setBackground(new Background(new BackgroundFill(Color.ORANGE, CornerRadii.EMPTY, Insets.EMPTY)));
+		this.outputView.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
+		this.statsView.setBackground(new Background(new BackgroundFill(Color.BLUE, CornerRadii.EMPTY, Insets.EMPTY)));
+		this.selectionView
+				.setBackground(new Background(new BackgroundFill(Color.GREEN, CornerRadii.EMPTY, Insets.EMPTY)));
+
+		this.combatView.setBorder(new Border(
+				new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+		this.outputView.setBorder(new Border(
+				new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+		this.statsView.setBorder(new Border(
+				new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+		this.selectionView.setBorder(new Border(
+				new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+	}
+
+}
--- Rollenspiel/src/application/Main.java
+++ Rollenspiel/src/application/Main.java
@@ -2,7 +2,6 @@ package application;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -10,9 +9,9 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root, 400, 400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Layout combatView = new Layout();
+
+			Scene scene = combatView.createScene();
 
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -24,6 +23,5 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 
-		System.out.println("Servus Nico");
 	}
 }
